87ba61a248d090b9cd95a6538e382bba4251f253
Shubham <arya.xoert@gmail.com>
2018-10-05 11:25:26 +0530

    #1

--- clientRoute/src/app.js
+++ clientRoute/src/app.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>this is new app</p>, document.getElementById("app"));
+const expense = () => <div>This is amazing</div>;
+const AddExpense = () => <div>this is add expense</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={expense} exact={true} />
+      <Route path="/create" component={AddExpense} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
